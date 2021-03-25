1835e45e2a85117d83a75418955c6c3fa2aeb5e7
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2021-03-25 17:07:19 +0500

    remove logs for prod fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-typechain");
 require("solidity-coverage");
 require("hardhat-preprocessor");
-require('hardhat-log-remover');
+const {removeConsoleLog} = require("hardhat-preprocessor");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,7 +22,9 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   solidity: "0.7.3",
-
+  preprocess: {
+    eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat' && hre.network.name !== 'localhost'),
+  },
 
 };
 
